22da8bd777414225a01697a768c78107ca2d4d14
Tojonantenaina8 <tojonantenaina08@gmail.com>
2023-01-16 00:58:46 +0300

    show first birthcerti

--- src/app/modules/dashboard/components/naissance/premiere-copie-voir/premiere-copie-voir.component.ts
+++ src/app/modules/dashboard/components/naissance/premiere-copie-voir/premiere-copie-voir.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-premiere-copie-voir',
@@ -10,6 +12,22 @@ export class PremiereCopieVoirComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  OpenCopie = false;
+  toggleModal(){
+    this.OpenCopie = !this.OpenCopie;
+  }
+  public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('copie.pdf');
+    });
   }
 
 }
