cfe8463446fbb4c93b915a558f5eeca2d1af7874
Tojonantenaina8 <tojonantenaina08@gmail.com>
2023-01-16 23:38:49 +0300

    feat: print first birth

--- src/app/modules/dashboard/components/naissance/premiere-copie-voir/premiere-copie-voir.component.ts
+++ src/app/modules/dashboard/components/naissance/premiere-copie-voir/premiere-copie-voir.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import jsPDF from 'jspdf';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
 
 @Component({
@@ -10,7 +10,7 @@ import html2canvas from 'html2canvas';
 export class PremiereCopieVoirComponent implements OnInit {
 
   constructor() { }
-
+  @ViewChild('htmlData') htmlData!: ElementRef;
   ngOnInit(): void {
   }
   OpenCopie = false;
@@ -18,7 +18,9 @@ export class PremiereCopieVoirComponent implements OnInit {
     this.OpenCopie = !this.OpenCopie;
   }
   public openPDF(): void {
+
     let DATA: any = document.getElementById('htmlData');
+    console.log(DATA);
     html2canvas(DATA).then((canvas) => {
       let fileWidth = 208;
       let fileHeight = (canvas.height * fileWidth) / canvas.width;
@@ -26,8 +28,18 @@ export class PremiereCopieVoirComponent implements OnInit {
       let PDF = new jsPDF('p', 'mm', 'a4');
       let position = 0;
       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-      PDF.save('copie.pdf');
+      PDF.save('angular-demo.pdf');
     });
   }
+  
+  printPage(){
+    var printContents = document.getElementById('htmlData')!.innerHTML;
+     var originalContents = document.body.innerHTML;
 
+     document.body.innerHTML = printContents;
+
+     window.print();
+
+     document.body.innerHTML = originalContents;
+  }
 }
