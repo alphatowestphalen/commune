3d6118dad8da93c1177f85342219244b764f73c8
Tojonantenaina8 <Tojonantenaina08@gmail.com>
2023-02-03 12:02:05 +0300

    refactor: consumme API Back

--- src/app/modules/dashboard/services/premiere-copie.service.ts
+++ src/app/modules/dashboard/services/premiere-copie.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,12 @@ import { Observable } from 'rxjs';
 export class PremiereCopieService {
 
 
-  private baseUrl = "http://localhost:3000/premiereCopie";
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
 
   getFirstCertificates(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+    return this.http.get(`${this.baseUrl}/premierCopies`);
   }
 }
