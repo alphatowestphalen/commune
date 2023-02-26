5d33603c249a8635c39bdef68a6fdfe8593de038
Tojonantenaina8 <tojonantenaina08@gmail.com>
2023-02-26 23:28:53 +0100

    feat: mariage component

---
+++ src/app/modules/dashboard/components/mariage/mariage/mariage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mariage',
+  templateUrl: './mariage.component.html',
+  styleUrls: ['./mariage.component.scss']
+})
+export class MariageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
