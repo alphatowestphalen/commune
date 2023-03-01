d9166a84056489a374f94b2c8d5c3066dc3c7f3a
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-03-01 15:27:41 +0300

    feat : add service add bulletin function

--- src/app/modules/dashboard/services/bulletin-naissance.service.ts
+++ src/app/modules/dashboard/services/bulletin-naissance.service.ts
@@ -1,5 +1,17 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../../../environments/environment';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
-export class BulletinNaissanceService {}
+export class BulletinNaissanceService {
+  constructor(private readonly http: HttpClient) {}
+  private baseURl: string = environment.baseUrl + '/bulletinNaissances';
+  async saveBulletin(sender: any, options: any): Promise<Observable<any>> {
+    options.showDataSaving();
+    return this.http.post(this.baseURl, sender.data, {
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    });
+  }
+}
