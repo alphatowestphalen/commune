4d5eeafcb60971f15f8e4710649d33f08ee3a843
Tojonantenaina8 <tojonantenaina08@gmail.com>
2023-03-15 04:56:24 +0100

    feat : compo board

---
+++ src/app/modules/features/components/scoped-b/scoped-b.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scoped-b',
+  templateUrl: './scoped-b.component.html',
+  styleUrls: ['./scoped-b.component.scss']
+})
+export class ScopedBComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
