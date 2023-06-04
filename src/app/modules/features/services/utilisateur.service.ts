ee47ee225b9d1f64b4d2c142f6ee024ee6aa1217
Nantenaina08 <tojonantenaina08@gmail.com>
2023-06-04 14:44:27 +0300

    feat: consume historic users

---
+++ src/app/modules/features/services/utilisateur.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilisateurService {
+
+    
+private baseURl = environment.baseUrl+'/users'
+private Url = environment.baseUrl
+
+constructor(private http: HttpClient) { }
+
+getAllUsers(size: number, page: number):Observable<any>{
+    return this.http.get(`${this.baseURl}?page=${page}&size=${size}`)
+}
+
+historiqueUser(){
+    return this.http.get(`${this.Url}/historiques`)
+}
+
+}
