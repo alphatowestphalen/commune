34451ca1119c2cbac59dcf5d7f93c7d295ea0e60
alphatowestphalen <alphato2021@gmail.com>
2023-09-07 17:11:09 +0300

    change branch

---
+++ src/app/model/bulletin/Buletin.interface.ts
@@ -0,0 +1,15 @@
+export interface BulletinNaicensse{
+    idBulletinNaissance: 0,
+    idPremierCopie: string;
+    type: string;
+    nomPersonne: string;
+    prenomsPersonne: string;
+    dateNaissPersonne: string;
+    lieuNaissPersonne: string;
+    nomPere: string;
+    prenomsPere: string;
+    nomMere: string;
+    prenomsMere: string;
+    dateCopie: string;
+    createdDate: "2023-09-07T13:21:55.708Z"
+  }
